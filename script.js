c162641dd9b991b316fde53b2236a4d698865b6f
adekola <tellaibrahimadekola@gmail.com>
2022-09-20 15:20:20 +0100

    first commit

--- script.js
+++ script.js
@@ -21,19 +21,14 @@ function empty_example(empty){
    let results = "";
    for (let i = 0; i < data[0].meanings.length; i++) {
        results += `<div class="container">
-       <b class="container" id="audio"><i class="fas fa-speaker"></i>
+    //  <b class="container" id="audio"><i class="fas fa-speaker"></i>
        <audio controls autoplay><source src = "${data[0].phonetics[0].audio}"></audio></b>
-       <h2>Pronunciation:<span id="trans">${data[0].phonetic}</span></h2><hr>
-       <h2 class="container" id="meaning">Part of Speech:<span>${data[0].meanings[i].partOfSpeech}</span></h2>
-                             <h3>Meaning:<span> ${data[0].meanings[i].definitions[0].definition}</span></h3>
-                                    <h2><span id="example"> ${empty_example(data[0].meanings[i].definitions[0].example)}</span></h2>
-                                    
-   
+       <h3>Pronunciation:<span id="trans">${data[0].phonetic}</span></h3><hr>
+       <h3 class="container" id="meaning">Part of Speech:<span>${data[0].meanings[i].partOfSpeech}</span></h3>
+     <h3>Meaning:<span> ${data[0].meanings[i].definitions[0].definition}</span></h3>
+     <h2><span id="example"> ${empty_example(data[0].meanings[i].definitions[0].example)}</span></h2>
    </div>`
    }
    show.innerHTML = results;
    })
-   
-   
-   
    }
